d42aae0b7d21b5bc57ad7f981e23f75dc3bd7714
okano masora <okano.masora.19@shizuoka.ac.jp>
2021-11-12 14:49:59 +0900

    実験2合格

--- src/lang/c/parse/Expression.java
+++ src/lang/c/parse/Expression.java
@@ -81,7 +81,7 @@ class ExpressionAdd extends CParseRule {
 		final int s[][] = {
 		//		T_err			T_int			T_pint
 			{	CType.T_err,	CType.T_err,	CType.T_err  },	// T_err
-			{	CType.T_err,	CType.T_int,	CType.T_err },	// T_int
+			{	CType.T_err,	CType.T_int,	CType.T_pint },	// T_int
 			{	CType.T_err,	CType.T_pint,	CType.T_err },	// T_pint
 		};
 		if (left != null && right != null) {
@@ -141,7 +141,7 @@ class ExpressionSub extends CParseRule {
 		//		T_err			T_int			T_pint
 			{	CType.T_err,	CType.T_err,	CType.T_err  },	// T_err
 			{	CType.T_err,	CType.T_int,	CType.T_err },	// T_int
-			{	CType.T_err,	CType.T_pint,	CType.T_err },	// T_pint
+			{	CType.T_err,	CType.T_pint,	CType.T_int },	// T_pint
 		};
 		if (left != null && right != null) {
 			left.semanticCheck(pcx);
