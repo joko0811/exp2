2ab8acc0a4ee0178783ee7c43b0333d538bd53c3
okano masora <okano.masora.19@shizuoka.ac.jp>
2021-12-06 16:33:49 +0900

    ex5 合格時

--- src/lang/c/parse/Program.java
+++ src/lang/c/parse/Program.java
@@ -48,7 +48,6 @@ public class Program extends CParseRule {
 				Statement statement = (Statement) statementList.get(i);
 				statement.codeGen(pcx);
 			}
-			o.println("\tMOV\t-(R6), R0\t; ProgramNode: 計算結果確認用");
 		}
 		o.println("\tHLT\t\t\t; ProgramNode:");
 		o.println("\t.END\t\t\t; ProgramNode:");
